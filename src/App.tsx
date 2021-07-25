9e044446c8b93d85c7649d84ed05879f091b7996
Meissam Rasouli <meissam.rasouli@gmail.com>
2021-07-25 05:24:05 +0430

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { ClickCounter } from '@components/ClickCounter';
+import './App.css';
+
+export const App = () => {
+    return (
+        <>
+            <ClickCounter />
+            {/* <img src={image}/> */}
+            <h1 className="app"> This is asdf a asdfadsf and its</h1>
+        </>
+    );
+};
