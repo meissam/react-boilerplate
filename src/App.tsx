d3c28784d58ec1e5b2d2e1b7f0b49582effc1c23
Meissam Rasouli <meissam.rasouli@gmail.com>
2021-08-17 08:24:17 +0430

    test path resolver

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
+import { Header } from '@components/layout';
 
 // App.js sets up the auth and routing.
 export const App = () => {
-    return <h1 className="app"> This is asdf a asdfadsf and its</h1>;
+    return (
+        <h1 className="app">
+            This is asdf a asdfadsf <Header /> and its
+        </h1>
+    );
 };
